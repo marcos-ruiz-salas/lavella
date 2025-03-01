57891da5c760ea312814e8edb8d002fef42b7727
Nex <marcosruizsalas@hotmail.com>
2025-03-01 11:40:19 +0100

    fix: noticeboard movement

--- src/components/NoticeBoard.tsx
+++ src/components/NoticeBoard.tsx
@@ -5,18 +5,5 @@ interface NoticeBoardParams {
 }
 
 export default function NoticeBoard({ children }: NoticeBoardParams) {
-    const noticeboardSection = document.getElementById("noticeboard");
-
-    let index = 0;
-    if (noticeboardSection) {
-        const images = noticeboardSection?.children ?? [];
-
-        function moveNotice() {
-            index = (index + 1) % images.length;
-            if (noticeboardSection) noticeboardSection.style.transform = `translateX(-${index * 100}%)`;
-        }
-
-        setInterval(moveNotice, 12000);
-    }
     return <section id="noticeboard_container" className="noticeboard_container">{children}</section>
 }
